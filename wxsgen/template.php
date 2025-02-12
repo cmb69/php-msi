96ddcebaf7a556b17915f2ae282ff18e045caac0
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 02:15:45 +0100

    Add Package element
    We can now `candle`.

--- wxsgen/template.php
+++ wxsgen/template.php
@@ -1,6 +1,8 @@
 <Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
  <Product Name="PHP" Id="<?=$product_code?>" UpgradeCode="34B67696-011A-46C7-94F8-FB450EF4CB0D"
           Language="1033" Codepage="1252" Version="<?=$version?>" Manufacturer="PHP Group">
-
+  <Package Id="*" Keywords="Installer" Description="PHP Installer"
+   Comments="Copyright © The PHP Group" Manufacturer="PHP Group"
+   InstallerVersion="200" Languages="1033" Compressed="yes" SummaryCodepage="1252"/>
  </Product>
 </Wix>
