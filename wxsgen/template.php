da75ff1b240c17193a96836de2f650dca80ea8a1
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 19:56:51 +0100

    Install shortcut for interactive shell

--- wxsgen/template.php
+++ wxsgen/template.php
@@ -10,16 +10,30 @@
       <Directory Id="<?=$program_files?>" Name="PFiles">
         <Directory Id="INSTALLDIR" Name="PHP"/>
       </Directory>
+      <Directory Id="ProgramMenuFolder" Name="Programs">
+        <Directory Id="ProgramMenuDir" Name="PHP">
+          <Component Id="ProgramMenuDir" Guid="D0131053-4C0D-4520-971F-42B520C3C8DD">
+            <RemoveFolder Id="ProgramMenuDir" On="uninstall"/>
+            <RegistryValue Root="HKCU" Key="Software\[Manufacturer]\[ProductName]" Type="string" Value="" KeyPath="yes"/>
+          </Component>
+        </Directory>
+      </Directory>
     </Directory>
+    <Icon Id="ico_php.exe" SourceFile="<?=$dir?>\php.exe"/>
 <?foreach ($dirs as [$id, $parent, $name]):?>
     <DirectoryRef Id="<?=$parent?>">
       <Directory Id="<?=$id?>" Name="<?=$name?>"/>
     </DirectoryRef>
 <?endforeach?>
     <Feature Id="fPhp">
+      <ComponentRef Id="ProgramMenuDir"/>
 <?foreach ($files as $i => [$file, $subdir, $guid]):?>
       <Component Id="cmp_<?=$i?>" Directory="<?=$subdir?>" Guid="<?=$guid?>">
-        <File KeyPath="yes" Source="<?=$dir?><?=$file?>"/>
+        <File KeyPath="yes" Source="<?=$dir?><?=$file?>">
+<?  if ($file === "\\php.exe"):?>
+          <Shortcut Id="sct0" Directory="ProgramMenuDir" Name="Interactive Shell" Icon="ico_php.exe" IconIndex="0" Advertise="yes"/>
+<?  endif?>
+        </File>
       </Component>
 <?endforeach?>
     </Feature>
