426daff2bf801fe91d1147e5c80992256e606d1a
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 02:06:52 +0100

    Dynamically generate product code

--- wxsgen/template.php
+++ wxsgen/template.php
@@ -1,5 +1,5 @@
 <Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
- <Product Name="PHP" Id="E85302BF-4D87-4633-AB9C-AAFA1ED4133F" UpgradeCode="34B67696-011A-46C7-94F8-FB450EF4CB0D"
+ <Product Name="PHP" Id="<?=$product_code?>" UpgradeCode="34B67696-011A-46C7-94F8-FB450EF4CB0D"
           Language="1033" Codepage="1252" Version="<?=$version?>" Manufacturer="PHP Group">
 
  </Product>
--- wxsgen/wxsgen.php
+++ wxsgen/wxsgen.php
@@ -1,7 +1,10 @@
 <?php
 
 echo "<?xml version=\"1.0\" encoding=\"windows-1252\"?>\n";
-render(["version" => "8.4.2"]);
+render([
+    "version" => "8.4.2",
+    "product_code" => gen_uuid(),
+]);
 
 function render(array $data) {
     array_walk_recursive($data, function (&$value) {
@@ -9,4 +12,14 @@ function render(array $data) {
     });
     extract($data);
     include __DIR__ . "/template.php";
+}
+
+function gen_uuid() {
+    $rand = random_bytes(16);
+    $rand[6] = chr(ord($rand[6]) & 0x0f | 0x40);
+    $rand[8] = chr(ord($rand[8]) & 0x3f | 0x80);
+    $uuid = strtoupper(bin2hex($rand));
+    return substr($uuid, 0, 8) . "-" . substr($uuid, 8, 4) . "-"
+        . substr($uuid, 12, 4) . "-" . substr($uuid, 16, 4) . "-"
+        . substr($uuid, 20, 12);
 }
