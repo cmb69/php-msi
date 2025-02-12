35e7bf4f7ba365d03628fa1614698c42d1f15604
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 14:22:15 +0100

    typo

--- wxsgen/template.php
+++ wxsgen/template.php
@@ -2,7 +2,7 @@
   <Product Name="PHP" Id="<?=$product_code?>" UpgradeCode="34B67696-011A-46C7-94F8-FB450EF4CB0D"
       Language="1033" Codepage="1252" Version="<?=$version?>" Manufacturer="PHP Group">
     <Package Id="*" Keywords="Installer" Description="PHP Installer"
-        Comments="Copyright Â© The PHP Group" Manufacturer="PHP Group"
+        Comments="Copyright © The PHP Group" Manufacturer="PHP Group"
         InstallerVersion="200" Languages="1033" Compressed="yes" SummaryCodepage="1252"/>
     <Media Id="1" Cabinet="php.cab" EmbedCab="yes"/>
     <Directory Id="TARGETDIR" Name="SourceDir">
