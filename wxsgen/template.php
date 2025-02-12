9a78120f41d57426059b2d991b6ddb35ded8d5d7
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 03:39:13 +0100

    Install into "proper" folder

--- wxsgen/template.php
+++ wxsgen/template.php
@@ -5,10 +5,14 @@
         Comments="Copyright © The PHP Group" Manufacturer="PHP Group"
         InstallerVersion="200" Languages="1033" Compressed="yes" SummaryCodepage="1252"/>
     <Media Id="1" Cabinet="php.cab" EmbedCab="yes"/>
-    <Directory Id="TARGETDIR" Name="SourceDir"/>
+    <Directory Id="TARGETDIR" Name="SourceDir">
+      <Directory Id="ProgramFilesFolder" Name="PFiles">
+        <Directory Id="INSTALLDIR" Name="PHP"/>
+      </Directory>
+    </Directory>
     <Feature Id="fPhp">
-      <Component Id="cmp_1" Directory="TARGETDIR" Guid="<?=$component_guid?>">
-        <File Id="fil6D063D02EE925FBDADCDD3EFEFB6D5DE" KeyPath="yes" Source="php\deplister.exe"/>
+      <Component Id="cmp_1" Directory="INSTALLDIR" Guid="<?=$component_guid?>">
+        <File KeyPath="yes" Source="php\deplister.exe"/>
       </Component>
     </Feature>
     <UIRef Id="WixUI_Minimal"/>
