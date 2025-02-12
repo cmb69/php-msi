58ae7abebf0151a907c39b19f16797366d1078a7
Christoph M. Becker <cmbecker69@gmx.de>
2025-02-12 01:44:20 +0100

    Basic render()

--- wxsgen/wxsgen.php
+++ wxsgen/wxsgen.php
@@ -1,6 +1,12 @@
 <?php
 
-$version = "8.4.2";
-
 echo "<?xml version=\"1.0\" encoding=\"windows-1252\"?>\n";
-include __DIR__ . "/template.php";
+render(["version" => "8.4.2"]);
+
+function render(array $data) {
+    array_walk_recursive($data, function (&$value) {
+        $value = htmlspecialchars($value);
+    });
+    extract($data);
+    include __DIR__ . "/template.php";
+}
